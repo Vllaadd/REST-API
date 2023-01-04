f6440a5b7d85d0c9751f3e5867d8547b85870583
Vllaadd <vlladdiimiir@gmail.com>
2023-01-04 04:41:08 -0500

    server created and  mongo set up

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+require('.env').config()
+
+const { config } = require('dotenv');
 const express = require('express');
 const app = express() 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/subscribers', {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 app.listen(3000, () => console.log('Server Started')); 
