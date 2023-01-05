89e60d88d58cf871c69b3ba57482f46962f14785
Vllaadd <vlladdiimiir@gmail.com>
2023-01-05 01:13:48 -0500

    schema craeted

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 //Getting All 
-router.get('/', (req, res) => {
-    res.send('This is for getting all');
+router.get('/', async(req, res) => {
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
 })
 //Getting One
 router.get('/:id', (req, res) =>{
