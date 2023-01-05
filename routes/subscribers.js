81f17e7025a85f5fe53f35272716dbfcc2dfaa9e
Vllaadd <vlladdiimiir@gmail.com>
2023-01-05 00:47:27 -0500

    creating routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,9 +2,24 @@ const express = require('express')
 const router = express.Router()
 
 //Getting All 
+router.get('/', (req, res) => {
+    res.send('This is for getting all');
+})
 //Getting One
+router.get('/:id', (req, res) =>{
+
+})
 //Creating One 
+router.post('/:id', (req, res) =>{
+    
+})
 //Updating One 
+router.patch('/', (req, res) => {
+
+})
 //Deleting One
+router.delete('/:id', (req, res)=>{
+
+})
 
 module.exports = router
